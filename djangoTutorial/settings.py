cbbc85c05cac006ff4a44245b4fb427ba78ea2df
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-04-16 17:27:23 +1000

    customise the heading of the admin site

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hfw-ogy&=xm!-8wbo2k0&2c6x=d#(_=m@%m1qk+0rrc&wg(+0u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
